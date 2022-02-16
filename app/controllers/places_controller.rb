815deb1d36906ecf94e3cdd980bffe008a868f86
Davidr1002 <david.rau@kellogg.northwestern.edu>
2022-02-16 22:51:35 +0000

    progress xx

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -24,7 +24,7 @@ class PlacesController < ApplicationController
       def destroy
         @place = Place.find(params["id"])
         @place.destroy
-        redirect_to "/companies"
+        redirect_to "/places"
       end
 
 
